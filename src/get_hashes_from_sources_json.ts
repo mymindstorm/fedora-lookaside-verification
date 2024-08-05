81234c637f22c4ba8f53a0363ca8f19c28399664
Brendan Early <mymindstorm@evermiss.net>
2024-08-05 01:51:00 -0500

    ftp and http downloads working!

--- src/get_hashes_from_sources_json.ts
+++ src/get_hashes_from_sources_json.ts
@@ -1,3 +1,79 @@
-export default async function getHashesFromSourcesJSON() {
+import { appendFile, readFile, writeFile } from "fs/promises";
+import { createHash } from "crypto";
+import { Readable } from "node:stream";
+import { finished } from "node:stream/promises";
+import { Client } from "basic-ftp";
+
+const SpecNameRegex = /(.+)\.spec$/;
+
+export default async function getHashesFromSourcesJSON(
+  path: string,
+  output: string,
+) {
+  await writeFile(output, "Source Package,Protocol,URL,SHA256,Error\n", {
+    flag: "w",
+  });
+
+  const sourcesJSON: Record<string, { error?: string; sources: string[] }> =
+    JSON.parse((await readFile(path)).toString());
+
+  for (const spec in sourcesJSON) {
+    // Get source package name
+    const specNameMatches = spec.match(SpecNameRegex);
+    if (specNameMatches === null) {
+      console.warn(`${spec} unable to be split into source package name!`);
+      continue;
+    }
+    const specName = specNameMatches[1];
+
+    // Get hash of files in sources
+    for (const rawURL of sourcesJSON[spec].sources) {
+      try {
+        const url = new URL(rawURL);
+
+        try {
+          console.log(rawURL);
+          const sha256 = createHash("sha256");
+
+          if (url.protocol === "http:" || url.protocol === "https:") {
+            const res = await fetch(url);
+            const resBody = res.body;
+            if (!res.ok) {
+              throw new Error("Response bad status code");
+            }
+
+            if (resBody === null) {
+              throw new Error("Response body null!");
+            }
+
+            // WEIRDNESS: If you do this in one line (like in get_sources_json_from_specs.ts), node just exits without any errors
+            const resStream = Readable.fromWeb(resBody);
+            resStream.pipe(sha256);
+            await finished(resStream);
+          } else if (url.protocol === "ftp:") {
+            const client = new Client();
+            await client.access({
+              host: url.host,
+            });
+            await client.downloadTo(sha256, url.pathname);
+          } else {
+            throw new Error("Unsupported protocol.");
+          }
+          await appendFile(
+            output,
+            `${specName},${url.protocol},${rawURL},${sha256.digest("hex")},\n`,
+          );
+        } catch (e) {
+          await appendFile(
+            output,
+            `${specName},${url.protocol},${rawURL},,${e}\n`,
+          );
+        }
+      } catch {
+        // no-op. we don't process anything with an invalid url
+      }
+    }
+  }
+
   return;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { version, name } from "../package.json";
 import { Command } from "commander";
 import getSourcesJSONFromSpecs from "./get_sources_json_from_specs";
+import getHashesFromSourcesJSON from "./get_hashes_from_sources_json";
 
 const program = new Command();
 program.name(name);
 program.version(version);
 
 program
-  .command("hash-from-specs")
+  .command("sources-from-specs")
   .description("Get the source URLS from spec files on the lookaside cache.")
   .argument(
     "[url-to-specs]",
@@ -15,12 +16,27 @@ program
     "https://src.fedoraproject.org/repo/rpm-specs-latest.tar.xz",
   )
   .option(
-    "-o, --output-csv <path>",
-    "Path to output csv. Existing files will be overwritten, missing directories will be created.",
-    "out/sources-from-specs.json",
+    "-o, --output-json <path>",
+    "Path to output sources JSON. Existing files will be overwritten, missing directories must exist or it will error out at the end.",
+    "sources-from-specs.json",
   )
-  .action(async (url: string, options: { outputCsv: string }) => {
-    await getSourcesJSONFromSpecs(url, options.outputCsv);
+  .action(async (url: string, options: { outputJson: string }) => {
+    await getSourcesJSONFromSpecs(url, options.outputJson);
+  });
+
+program
+  .command("hash-from-sources")
+  .description(
+    "Get the hash of every remote file in referenced in the output of sources-from-specs's JSON file. Invalid URLs are ignored. http://, https://, and ftp:// are supported.",
+  )
+  .argument("<path-to-json>", "Path to output of sources-from-specs")
+  .option(
+    "-o, --output-csv <path>",
+    "Path to output CSV. Existing files will be overwritten, missing directories must exist or it will error out at the end.",
+    "hashes.csv",
+  )
+  .action(async (path: string, options: { outputCsv: string }) => {
+    await getHashesFromSourcesJSON(path, options.outputCsv);
   });
 
 program.parse(process.argv);
